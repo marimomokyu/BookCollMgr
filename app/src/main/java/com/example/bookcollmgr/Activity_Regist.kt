59df582115e42da4060f7883dc46e3fc900e7283
Tsutsumi <soraurikyufmof@gmail.com>
2021-04-20 16:26:41 +0900

    登録画面のDB登録処理を実装

--- app/src/main/java/com/example/bookcollmgr/Activity_Regist.kt
+++ app/src/main/java/com/example/bookcollmgr/Activity_Regist.kt
@@ -2,9 +2,17 @@ package com.example.bookcollmgr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
+import android.text.format.DateFormat
+import android.util.Log
+import android.widget.Toast
+import com.facebook.stetho.Stetho
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider
 import io.realm.Realm
 import io.realm.kotlin.createObject
 import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity__regist.*
+import java.util.*
 
 class Activity_Regist : AppCompatActivity() {
     private val tag = "hasBook"
@@ -15,13 +23,40 @@ class Activity_Regist : AppCompatActivity() {
         setContentView(R.layout.activity__regist)
         realm = Realm.getDefaultInstance()
 
-        //TODO ここにボタンが押された時の処理を書く
+        // stetho-realmの初期化
+        Stetho.initialize(
+            Stetho.newInitializerBuilder(this)
+                .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                .build())
 
+
+        //保存ボタン押下時処理を書く
+        saveBtn.setOnClickListener {
+            var Name: String = ""
+
+            //名前
+            if(!BookNameedit.text.isNullOrEmpty()){
+                Name = BookNameedit.text.toString()
+            }
+
+            //TODO 他のテキストボックスが追加されたら、名前登録処理を参考に実装する
+
+            //realm登録処理
+            realm.executeTransaction {
+                val maxId = realm.where<hasBook>().max("id")
+                val nextId = (maxId?.toLong() ?: 0L) + 1L
+                val hasBook = realm.createObject<hasBook>(nextId)
+                hasBook.dateTime = Date()
+                hasBook.bookname = Name
+            }
+            Toast.makeText(applicationContext,"登録しました",Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        //realm.close()
-
+        realm.close()
     }
 }
