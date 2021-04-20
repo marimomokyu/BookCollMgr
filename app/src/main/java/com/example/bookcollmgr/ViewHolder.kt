c7e1d7029916c8fb943cec1f68b34c05a01bc7ee
Tsutsumi <soraurikyufmof@gmail.com>
2021-04-20 18:05:32 +0900

    recycleeview実装途中

---
+++ app/src/main/java/com/example/bookcollmgr/ViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.bookcollmgr
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import android.widget.Toast
