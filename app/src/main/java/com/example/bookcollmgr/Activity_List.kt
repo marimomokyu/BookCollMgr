8dd7620defe9a977e350bce02e84bc864f1692a2
Tsutsumi <soraurikyufmof@gmail.com>
2021-05-21 12:02:58 +0900

    no message

--- app/src/main/java/com/example/bookcollmgr/Activity_List.kt
+++ app/src/main/java/com/example/bookcollmgr/Activity_List.kt
@@ -5,43 +5,31 @@ import android.os.Bundle
 import android.content.Intent
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.realm.Realm
-import io.realm.kotlin.where
+import io.realm.Sort
 import kotlinx.android.synthetic.main.activity__list.*
 
 class Activity_List : AppCompatActivity() {
     private lateinit var realm: Realm
+    private lateinit var adapter : CustomRecyclerViewAdapter
+    private lateinit var layoutManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity__list)
-        realm = Realm.getDefaultInstance()
-
-        // データ削除
-        //deleteRealm()
-
-        val bookList = realm.where<hasBook>().findAll()
-
-        val adapter = ListAdapter(this, bookList,true)
-
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        realm.close()
-    }
+    //override fun onStart(){
+    //    super.onStart()
+    //    val realmResults = realm.where(hasBook::class.java)
+    //        .findAll()
+    //         .sort("id",Sort.DESCENDING)
+    //    layoutManager = LinearLayoutManager(this)
+    //    recyclerView.layoutManager = layoutManager
+    //    recyclerView.adapter = this.adapter
+    //}
 
-    private fun deleteRealm() {
-        val target = realm.where<hasBook>().findAll()
-
-        // トランザクションして削除
-        realm.executeTransaction {
-            target.deleteFromRealm(0)
-        }
-    }
 
     //変更・修正画面への遷移。最終的にはリストをタップで遷移するようにする予定。
     //今は単純にボタンタップで遷移するようにする
